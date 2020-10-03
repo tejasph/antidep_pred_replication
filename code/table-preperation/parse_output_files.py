271546705908651c60bb17868735d60a5a87a356
John-Jose Nunez <JJnunez11@gmail.com>
2020-10-03 16:45:30 -0700

    Added code to generate the supplmentary tables

---
+++ code/table-preperation/parse_output_files.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct  3 12:48:28 2020
+
+@author: jjnun
+"""
+
+import os
+import re
+
+##output_dir = r'C:\Users\jjnun\Documents\Sync\Research\1_CANBIND_Replication\Paper Submission\DataForFigures\Table3_Replication/'
+output_dir = r'C:\Users\jjnun\Documents\Sync\Research\1_CANBIND_Replication\Paper Submission\DataForFigures\Table4_ExternalValidation/'
+mean_re = re.compile('is: 0\.\d{1,4}')
+mean_re = re.compile('a')
+sd_re = re.compile('Deviation: 0\.\d{1,4}')
+
+metrics = ['Mean accuracy', 'Mean balanced accuracy', 'Mean AUC', 'Mean sensitivty', 'Mean specificty', 'Mean precision', 'Mean f1','Mean true positive','Mean false positive','Mean true negative', 'Mean false negative'] 
+
+w = open(output_dir + 'parsed_outputs.txt', 'w+')
+
+for filename in os.listdir(output_dir):
+    if filename.endswith('.txt'):
+        w.write(filename + '\n')
+        out_str = ''
+        f = open(output_dir + '/' + filename, 'r')
+        for line in f.readlines():
+            for metric in metrics:
+                if metric in line:
+                    mean_match = re.search(r'is: (0\.\d{1,6})', line)
+                    sd_match = re.search(r'Deviation: (0\.\d{1,8})', line)
+                    out_str = out_str + mean_match.group(1) + "\t" + sd_match.group(1) + "\t"
+                    
+        f.close()
+        w.write(out_str + '\n')
+        
+w.close()
---
+++ code/table-preperation/t_test_grids.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 18 10:14:57 2020
+
+@author: jjnun
+"""
+from scipy.stats import ttest_ind
+from result_dicts import TABLE3_BACCS, TABLE3_AUCS, TABLE4_BACCS, TABLE4_AUCS
+
+def write_t_test_grid(output_dir, baccs, aucs):
+    output_path = output_dir + 't_test_grid.csv'
+    f = open(output_path, 'w+')
+    f.write('T_test Grid\n Balanced Accuracy two-tailed two-sided t-tests\n,')
+    for bacc in baccs:
+        f.write(bacc + ',')
+
+    for bacc in baccs:
+        f.write('\n' + bacc + ',')    
+        for bacc2 in baccs:
+            p_value = ttest_ind(baccs[bacc],baccs[bacc2]).pvalue
+            f.write(f'{p_value},')
+    
+    f.write('\n\n AUC two-tailed two-sided t-tests\n,')
+    for auc in aucs:
+        f.write(auc + ',')
+
+    for auc in aucs:
+        f.write('\n' + auc + ',')    
+        for auc2 in aucs:
+            p_value = ttest_ind(aucs[auc],aucs[auc2]).pvalue
+            f.write(f'{p_value},')
+
+    f.close()
+
+
+table3_path = r'C:\Users\jjnun\Documents\Sync\Research\1_CANBIND_Replication\Paper Submission\DataForFigures\Table3_Replication/'
+table4_path = r'C:\Users\jjnun\Documents\Sync\Research\1_CANBIND_Replication\Paper Submission\DataForFigures\Table4_ExternalValidation/'
+
+write_t_test_grid(table3_path, TABLE3_BACCS, TABLE3_AUCS)
+write_t_test_grid(table4_path, TABLE4_BACCS, TABLE4_AUCS)
+
