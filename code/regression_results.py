42af60cd895533888e78c3e4a5e7e32ff740d3f8
tejasph <tphaterp@gmail.com>
2021-08-09 10:52:31 -0700

    added experiment variable checks

--- code/regression_results.py
+++ code/regression_results.py
@@ -11,11 +11,38 @@ Run Experiment script
 Change variables and experiment name using this script. 
 """
 
+
+#########################################
+# Instructions
+# There are a few variable options that can be used/altered in each experiment. See below for list of options:
+#
+regressor_options = ['rf', 'sgdReg', 'gbdt'] # which regressor model to use
+X_path_options = ['X_train_norm_over', 'X_train_norm'] # whether to use overlapping features or full-set features
+y_proxy_options = ['score_change', 'final_score']  # what the target for the regressors will be
+
 if __name__ == "__main__":
 
     if True:
-        exp_name = "testing_replication"
+        exp_name = "test"
         out_path = os.path.join(REG_RESULTS_DIR, exp_name)
+
+        runs = 10
+        regressors = ["rf"]
+        X_paths = ["X_train_norm_over"]
+        y = "y_train"
+        y_proxies = ["final_score"]
+
+        if all(option in regressor_options for option in regressors):
+            print("Validated Regressor Options")
+        else: raise Exception(f"Invalid regressor option. Valid options are {regressor_options}")
+
+        if all(option in X_path_options for option in X_paths):
+            print("Validated X_path")
+        else: raise Exception(f"Invalid X_path option. Valid options are {X_path_options}")
+
+        if all(option in y_proxy_options for option in y_proxies):
+            print("Validated y_proxy")
+        else: raise Exception(f"Invalid y_proxy option. Valid options are {y_proxy_options}")
 
         # Makes sure not to overwrite any files
         if os.path.isdir(out_path):
@@ -23,12 +50,9 @@ if __name__ == "__main__":
         else:
             os.mkdir(out_path + "/")
 
-        runs = 10
-        regressors = ["rf"]
-        X_paths = ["X_train_norm_over"]
-        y = "y_train"
-        y_proxies = ["final_score"]
+
     
+        
 
         exp_summary = {'model':[],'target':[], 'features':[], 'train_RMSE':[], 'valid_RMSE':[],
                             'CV_train_resp_bal_acc':[], 'CV_valid_resp_bal_acc':[],  'CV_train_rem_bal_acc':[], 'CV_valid_rem_bal_acc':[] }
@@ -36,6 +60,7 @@ if __name__ == "__main__":
         for regressor in regressors:
             for y_proxy in y_proxies:
                 for X_path in X_paths:
+
                     df_filename = "{}_{}_{}".format(regressor, X_path, y_proxy)
                     run_results = RunRegRun(regressor, X_path, y, y_proxy, out_path,  runs)
                     test_results = evaluate_on_test(regressor, X_path, y_proxy, out_path)
